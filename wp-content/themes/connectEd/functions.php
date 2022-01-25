fb5da59b250058583a4c576c8fbde6ebd21f2c27
David Sudarma <david@26am.com>
2022-01-25 15:40:28 -0500

    Theme Start

---
+++ wp-content/themes/connectEd/functions.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementorChild
+ */
+
+/**
+ * Load child theme css and optional scripts
+ *
+ * @return void
+ */
+function hello_elementor_child_enqueue_scripts() {
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		'1.0.0'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+
+
+// Remove Page Title
+function ele_disable_page_title( $return ) {
+   return false;
+}
+add_filter( 'hello_elementor_page_title', 'ele_disable_page_title' );
